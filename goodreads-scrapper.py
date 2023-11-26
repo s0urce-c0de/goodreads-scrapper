f625431551a7a5fa606f86f25236c79792eb2e4e
s0urce-c0de <email.kanavg@gmail.com>
2023-11-26 08:31:48 -0800

    update

--- goodreads-scrapper.py
+++ goodreads-scrapper.py
@@ -26,7 +26,7 @@ def internet_connection(server: str = "www.google.com", port: int = 80):
 def validate(url: str):
   url_regex="^(https?://)?(www\.)?goodreads\.com(/[^/]{2}(-[^/]{2})?)?/book/show/\d+$"
   if not re.match(url_regex, url):
-    click.echo(f'\x1b[31;1mInvalid  "{url}". Must match regex "{url_regex}"\x1b[0m')
+    click.echo(f'\x1b[31;1mInvalid book "{url}". Must match regex "{url_regex}"\x1b[0m')
     sys.exit(1)
   return url
 
@@ -79,7 +79,7 @@ def _real_main(url: str, UserAgent = None):
 
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
 @click.argument("book", type=str)
-def main( str):
+def main(book: str):
   """
   Get information on over 1,000,000+ books right from the terminal.
   """
